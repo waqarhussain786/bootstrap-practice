67c6322c4753f0b8aab9fa9f647f7adecc5d13c9
waqarhussain786 <wh751212@gmail.com>
2023-01-15 19:30:52 +0500

    bootstrap

--- src/firt-bootstrap.jsx
+++ src/firt-bootstrap.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-// import { Button } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+import { Button, Stack, Badge, Alert } from 'react-bootstrap';
+// import Button from 'react-bootstrap/Button';
 const FirtBootstrap = () => {
     return (
         <div>
             <div>
-                <Button>hello</Button>
+                <Stack direction="horizontal" gap={2}>
+                    <Button as="a" variant="primary">
+                        Button as link
+                    </Button>
+                    <Button as="a" variant="success">
+                        Button as link
+                    </Button>
+                </Stack>
+                <div>
+                    <h1>
+                        Example heading{' '}
+                        <Badge bg="secondary" as="Button">
+                            New
+                        </Badge>
+                    </h1>
+                </div>
+                <div>
+                    <Alert dismissible variant="danger">
+                        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                        <p>
+                            Change this and that and try again.
+                        </p>
+                    </Alert>
+                </div>
             </div>
             <div class="jumbotron text-center">
                 <h1>My First Bootstrap Page</h1>
