92e6c38d6a03668d263a4cd07371f8e657e2a697
waqarhussain786 <wh751212@gmail.com>
2023-01-15 19:37:23 +0500

    add alart

--- src/firt-bootstrap.jsx
+++ src/firt-bootstrap.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState } from 'react';
 import { Button, Stack, Badge, Alert } from 'react-bootstrap';
 // import Button from 'react-bootstrap/Button';
 const FirtBootstrap = () => {
+    const [show, setShow] = useState(true);     
     return (
         <div>
             <div>
@@ -33,6 +35,24 @@ const FirtBootstrap = () => {
             <div class="jumbotron text-center">
                 <h1>My First Bootstrap Page</h1>
                 <p>Resize this responsive page to see the effect!</p>
+            </div>
+            <div>
+                <Alert show={show} variant="success">
+                    <Alert.Heading>How's it going?!</Alert.Heading>
+                    <p>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+                        lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+                        fermentum.
+                    </p>
+                    <hr />
+                    <div className="d-flex justify-content-end">
+                        <Button onClick={() => setShow(false)} variant="outline-success">
+                            Close me y'all!
+                        </Button>
+                    </div>
+                </Alert>
+
+                {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
             </div>
 
             <div class="container">
