0ad63bc1d0aeea4af89e9f31e1833a348a1fcc0a
waqarhussain786 <wh751212@gmail.com>
2023-01-18 18:24:01 +0500

    navBar

--- src/navbar-bootstrap.jsx
+++ src/navbar-bootstrap.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 // import { Button, Stack, Badge, Alert } from 'react-bootstrap';
 import { Nav, Navbar, Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavbarBootstrap = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
+            <Navbar bg="dark" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">Link</Nav.Link>
+                            <NavDropdown title="Link" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#" disabled>Link</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
