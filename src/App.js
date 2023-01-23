0bb702ecb0a6140eecf2891aee7605769c8bcf4c
waqarhussain786 <wh751212@gmail.com>
2023-01-23 17:49:34 +0500

    @media-querry{}

--- src/App.js
+++ src/App.js
@@ -3,70 +3,95 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className = "row">
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
-      <div className = "col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
-      <div className = "col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
-      <div className = "col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
-      <div className = "col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
-      <div className = "col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
-      <div className = "col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
-      <div className = "col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
-      <div className = "col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
-      <div className = "col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
-      <div className = "col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-5 border border-primary rounded h6 text-center">.col-sm-5</div>
-      <div className = "col-sm-7 border border-primary rounded h6 text-center">.col-sm-7</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-6 border border-primary rounded h6 text-center">.col-sm-6</div>
-      <div className = "col-sm-6 border border-primary rounded h6 text-center">.col-sm-6</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-7 border border-primary rounded h6 text-center">.col-sm-7</div>
-      <div className = "col-sm-5 border border-primary rounded h6 text-center">.col-sm-5</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-8 border border-primary rounded h6 text-center">.col-sm-8</div>
-      <div className = "col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-9 border border-primary rounded h6 text-center">.col-sm-9</div>
-      <div className = "col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-10 border border-primary rounded h6 text-center">.col-sm-10</div>
-      <div className = "col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-11 border border-primary rounded h6 text-center">.col-sm-11</div>
-      <div className = "col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
-    </div>
-    <div className = "row">
-      <div className = "col-sm-12 border border-primary rounded h6 text-center">.col-sm-12</div>
-    </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+        </div>
+      </div>
+
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
+          <div className="col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
+          <div className="col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
+          <div className="col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
+          <div className="col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
+          <div className="col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
+          <div className="col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
+          <div className="col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
+          <div className="col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
+          <div className="col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
+          <div className="col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-5 border border-primary rounded h6 text-center">.col-sm-5</div>
+          <div className="col-sm-7 border border-primary rounded h6 text-center">.col-sm-7</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-6 border border-primary rounded h6 text-center">.col-sm-6</div>
+          <div className="col-sm-6 border border-primary rounded h6 text-center">.col-sm-6</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-7 border border-primary rounded h6 text-center">.col-sm-7</div>
+          <div className="col-sm-5 border border-primary rounded h6 text-center">.col-sm-5</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-8 border border-primary rounded h6 text-center">.col-sm-8</div>
+          <div className="col-sm-4 border border-primary rounded h6 text-center">.col-sm-4</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-9 border border-primary rounded h6 text-center">.col-sm-9</div>
+          <div className="col-sm-3 border border-primary rounded h6 text-center">.col-sm-3</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-10 border border-primary rounded h6 text-center">.col-sm-10</div>
+          <div className="col-sm-2 border border-primary rounded h6 text-center">.col-sm-2</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-11 border border-primary rounded h6 text-center">.col-sm-11</div>
+          <div className="col-sm-1 border border-primary rounded h6 text-center">.col-sm-1</div>
+        </div>
+      </div>
+      <div className="row2">
+        <div className='row2-items'>
+          <div className="col-sm-12 border border-primary rounded h6 text-center">.col-sm-12</div>
+        </div>
+      </div>
     </>
   );
 }
