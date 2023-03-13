82ac5f35da43613fac7947f0a24688fd11735c41
waqarhussain786 <wh751212@gmail.com>
2023-03-13 18:50:30 +0500

    new-branch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FirtBootstrap from './first-bootstrap.jsx';
-// import NavbarBootstrap from './navbar-bootstrap.jsx';
+import NavbarBootstrap from './navbar-bootstrap.jsx';
 import CollapsibleExample from './dark-navbar';
 import Img from './img';
 // import IfElse from './if-else';
@@ -15,8 +15,8 @@ root.render(
     <CollapsibleExample/>
     {/* <IfElse /> */}
     <Function />
-    {/* <NavbarBootstrap /> */}
-    <Img />
+    <NavbarBootstrap />
+    {/* <Img /> */}
     <FirtBootstrap />
     <App />
   </React.StrictMode>
